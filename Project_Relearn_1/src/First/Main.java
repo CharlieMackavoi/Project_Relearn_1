40e04860c38045352449607f2d72422c415e14c9
bernhardt <nicholasjbernhardt@gmail.com>
2013-02-04 19:46:00 -0600

    The most basic of programs.

--- Project_Relearn_1/src/First/Main.java
+++ Project_Relearn_1/src/First/Main.java
@@ -1,5 +1,14 @@
 package First;
 
 public class Main {
+	public static void main (String [] args){
+		
+	
 	//Simply here to test EGit commit functionality. 
+	String x = "Hello";
+	String y = "World";
+	
+	System.out.println(x + " " + y);
+	}
+	
 }
