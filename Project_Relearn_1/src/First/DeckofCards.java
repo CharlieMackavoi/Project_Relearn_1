fd79eec48c0f004de93bde68db228106611ce0d5
bernhardt <nicholasjbernhardt@gmail.com>
2013-02-08 14:00:26 -0600

    Completed the shuffle method

--- Project_Relearn_1/src/First/DeckofCards.java
+++ Project_Relearn_1/src/First/DeckofCards.java
@@ -1,11 +1,11 @@
 package First;
 
 public class DeckofCards {
-	//Creates a shuffled deck of cards
-	private static final int NCards = 52;
 	
+	private static final int NCards = 52;
 	private Card[] deckOfCards;
 	
+	//Constructor method to create a new deck
 	public DeckofCards(){
 		deckOfCards = new Card[NCards];
 		
@@ -19,6 +19,7 @@ public class DeckofCards {
 		
 	}
 	
+	//Gets a card from a position in the deck
 	public String getCard(int number){
 		String card;
 		
@@ -26,6 +27,34 @@ public class DeckofCards {
 		
 		
 		return card;
+	}
+	
+	//method to shuffle a created deck
+	public void shuffleDeck(){
+		Card temp;
+		int x;
+		
+		
+		//3 levels of shuffle in each pass of the loop
+		for(int counter = 0; counter < 4000000; counter++){
+			x = (int) (Math.random() * 50 + 1);
+			
+			//Random elements are switched in the array 
+			temp = deckOfCards[x];
+			deckOfCards[x] = deckOfCards[x+1];
+			deckOfCards[x+1] = temp;
+			
+			//The first element is switched with a random element
+			temp = deckOfCards[0];
+			deckOfCards[0] = deckOfCards[x % 7];
+			deckOfCards[x % 7] = temp;
+			
+			//The last element is switched with a random element
+			temp = deckOfCards[51];
+			deckOfCards[51] = deckOfCards[x % 9];
+			deckOfCards[x % 9] = temp;
+		}
+		
 	}
 	
 	
